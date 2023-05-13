a56cdb0619bb71cd8831a6b9fe2130aabde27c5e
Manuel Ortega Ruiz <manuellortegaruiz@gmail.com>
2023-05-13 12:25:36 -0500

    Agregando elementos y estructura

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import './App.css';
 
 function App() {
-  return <div className='App'>mi calculadora</div>;
+  return (
+    <div className='container'>
+      <h1>Calculadora App</h1>
+      <div className='display'>1235646</div>
+      <div className='controlsMemory'>
+        <button className='controlsMemory__clear'>CE</button>
+        <button className='controlsMemory__save'>SV</button>
+        <button className='controlsMemory__memory'>ME</button>
+        <button className='controlsMemory__delete'>DE</button>
+      </div>
+      <div className='controls'>
+        <div className='controls__keys'>
+          <button className='controls__keys__key_7'>7</button>
+          <button className='controls__keys__key_8'>8</button>
+          <button className='controls__keys__key_9'>9</button>
+          <button className='controls__keys__key_4'>4</button>
+          <button className='controls__keys__key_5'>5</button>
+          <button className='controls__keys__key_6'>6</button>
+          <button className='controls__keys__key_1'>1</button>
+          <button className='controls__keys__key_2'>2</button>
+          <button className='controls__keys__key_3'>3</button>
+          <button className='controls__keys__key_point'>.</button>
+          <button className='controls__keys__key_0'>0</button>
+          <button className='controls__keys__key_backspace'>Erase</button>
+        </div>
+        <div className='controls__functions'>
+          <button className='addition'>+</button>
+          <button className='substraction'>-</button>
+          <button className='multiplication'>x</button>
+          <button className='division'>/</button>
+          <button className='equal'>=</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
