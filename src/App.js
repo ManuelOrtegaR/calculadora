788292180b0df99f3ec8b45545a2a943a8da62ef
Manuel Ortega Ruiz <manuellortegaruiz@gmail.com>
2023-05-16 21:51:37 -0500

    commit avance corregir funcion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,87 @@
+import { useState } from 'react';
 import './App.css';
 
+const inputPanel = [7, 8, 9, 4, 5, 6, 1, 2, 3, '.', 0, 'back'];
+const displayText = '';
+const memoryData = [];
+const data = '0';
+const activeSign = null;
+
 function App() {
+  const inputOperators = [
+    { signo: '+' },
+    { signo: '-' },
+    { signo: '*' },
+    { signo: '/' },
+    { signo: '=' },
+  ];
+  const [display, setDisplay] = useState(displayText);
+  const [dataValue, setDataValue] = useState(data);
+  const [operatorSign, setOperatorSign] = useState(activeSign);
+  function Buttons() {
+    return inputPanel.map(function (elemento) {
+      return (
+        <button
+          className='col-3 controlPanel__Button controlPanel__Number'
+          onClick={printNumber}
+        >
+          {elemento}
+        </button>
+      );
+    });
+  }
+  function Operators() {
+    return inputOperators.map(function (elemento) {
+      const sigName = elemento.signo;
+      return (
+        <button
+          className='col-5 controlPanel__Button controlPanel__Number'
+          onClick={operations}
+        >
+          {sigName}
+        </button>
+      );
+    });
+  }
+
+  function printNumber(event) {
+    const displayValue = event.target.innerText;
+    if (displayValue === 'back') {
+      setDisplay(display.slice(0, -1));
+    } else {
+      if (displayValue === '.' && !display) {
+        setDisplay(display + '0.');
+      } else {
+        if (displayValue === '.' && display.includes('.') === false) {
+          setDisplay(display + '.');
+        } else {
+          if (displayValue !== '.') {
+            setDisplay(display + displayValue);
+          }
+        }
+      }
+    }
+  }
+
+  function evaluaArimetica(fn) {
+    return new Function('return ' + fn)();
+  }
+
+  function operations(event) {
+    const sign = event.target.innerText;
+    if (display && sign !== '=') {
+      const operation = display + sign;
+      const result = evaluaArimetica(operation);
+      setDataValue(result);
+      console.log(dataValue);
+      setDisplay('');
+    } else {
+      const operation = dataValue + operatorSign + display;
+      const result = evaluaArimetica(operation);
+      setDisplay(result);
+    }
+  }
+
   return (
     <div className='container'>
       <div className='aplication'>
@@ -11,7 +92,7 @@ function App() {
           />
           Calculadora
         </h1>
-        <div className='display'>Esto es un texto</div>
+        <div className='display'>{display}</div>
         <div className='memoryControl'>
           <button className='memoryControl__Button memory-Control__clear'>
             CE
@@ -26,95 +107,13 @@ function App() {
             DE
           </button>
         </div>
-        <div className='controlPanel'>
-          <button className='controlPanel__Operator controlPanel__Button'>
-            <img
-              className='controlPanel__ImgButton'
-              src={require('./img/charater C.png')}
-            />
-          </button>
-          <button className='controlPanel__Operator controlPanel__Button'>
-            <img
-              className='controlPanel__ImgButton'
-              src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAABMElEQVR4nO2UQUoDMRiFv5WeQAXHWepNeiZ7ESld240UV7Y3KCJuWyi4agvtyoIO3VcCrxCGTCaZjF3NB2Hgn+R/k+TNg46OOC6Ad2BGAnfAGCiADTAEbmrW9IEjME8R3auJPXZAVrEmBw6a12sqPFaDNwmZMVFtVLPGPBtTqElW2tFp12V6enfQvMYcNULqxlBL1R9TRGOFT4b6Ai5J5FfNjMl8R92KoWxe1WwicTOmqj23bSibB+Db8Tttgdu2DVXGOPpFx74CnoArh6HMHZ+NvkSX+ohy0hW6rvs2RXOHoaqSbu9JumhchvIlnbmuZKoM5Uu6n1RRX0LFBE40voT6N+G8JqF2gUkXTV1CjQKTLoqQhMqsXVclXTSLwIS6BgbA2pF0jfgEPqyE6ugghD9WEYrI0h9bwAAAAABJRU5ErkJggg=='
-            />
-          </button>
-          <button className='controlPanel__Operator controlPanel__Button'>
-            <img
-              className='controlPanel__ImgButton'
-              src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAACXBIWXMAAAsTAAALEwEAmpwYAAABX0lEQVR4nO3ZP0iVURgH4Cc0cXGotjYDJwdnqbGgxSUQmtpsFdpcxFHcBBenwEHcbY+gSXJ0aGhoCXJpCYKgP1z4vrhI0b2l3I/v/T1whrud38v9znnPOURERERERERE/L8p7CpqDsf4oaB5nDXhyxVgGR+HwpcqwGN8uRC+RAGuYQvffxO+9wWYxeEfgve+ALdx8pfwvS3AEt6PEL6XBXiEzyOG710BNvBtjPCDcY53OMUrvMAR9rGDTTzDGlbxEHebf9kd3MT1SQefwfMxg1/FGGyzH5pG6w1eNx3nQdN2bzc70jqeYAX3cQ+Lzbp1Y9zwt/CyA+Evc4xsAW87MOGJFOABPnVgshMpwFN87cBEUwD5BExsDVB9EWyV3gb72AgNTrD/bKNqKzys9GGoVfo43Cp9IdIqfSXWKn0pOqzstfiw0g8jrdJPY63Sj6Otaez9+hURERERERERoVt+Ak5unhI8HLDTAAAAAElFTkSuQmCC'
-            />
-          </button>
-          <button className='controlPanel__Operator controlPanel__Button'>
-            <img
-              className='controlPanel__ImgButton'
-              src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAXUlEQVR4nO3VMQqAMAxG4Xc8o/dfW2h7D0Wo0E0HGyL+H2R+S0JAJJgNaEAFzDNcgb1P+UXYevyMLp5h+a4VyMPGvjX57t7LhOg1LWTYgDQhmnTv8pT+sRvTP5awDufydEXZKLvNAAAAAElFTkSuQmCC'
-            />
-          </button>
-          <button className='controlPanel__Number controlPanel__Button'>
-            9
-          </button>
-          <button className='controlPanel__Number controlPanel__Button'>
-            8
-          </button>
-          <button className='controlPanel__Number controlPanel__Button'>
-            7
-          </button>
-          <button className='controlPanel__Number controlPanel__Button'>
-            <img
-              className='controlPanel__ImgButton'
-              src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAv0lEQVR4nO2VXQrCMBAGxwtalZY86NkVLP6hN6hQCaQgJdXdsFHEDOQt+02TbBooFP6JBrgBB2ChqFsCJ+AK1FrpLBT2YXSAE9S5MHeo8xlqLk8BEvlY6sc5RbyKBE3JY9IuZCRRRwLvwGbUC7E561SpRJ5N+m4rpUdhvvJsK5XKs0qZONNYw31E2ueUu280V/Piykjuubl0wFxeGfwyfYaa1uCR2KeIjwbPos9QU4Uv3iq3zM/dhdp5irhQ+E0ekyummbane5EAAAAASUVORK5CYII='
-            />
-          </button>
-          <button className='controlPanel__Number controlPanel__Button'>
-            6
-          </button>
-          <button className='controlPanel__Number controlPanel__Button'>
-            5
-          </button>
-          <button className='controlPanel__Number controlPanel__Button'>
-            4
-          </button>
-          <button className='controlPanel__Number controlPanel__Button'>
-            <img
-              className='controlPanel__ImgButton'
-              src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAc0lEQVR4nO2UMQqAMAxF3/Gsg3j/wULVQS8Rl05FpGATpeTBX/MgCR+cnzMDJ3AAk6V4ByRnsxRLERerIb5qK6SbVY9AvBn8NhEIT+KkIK1quPSVOACLgnQBBhrQz1fX4mIzxG9sRSzKwYwArDlNGslBiwtYs6fhOILl9QAAAABJRU5ErkJggg=='
-            />
-          </button>
-          <button className='controlPanel__Number controlPanel__Button'>
-            3
-          </button>
-          <button className='controlPanel__Number controlPanel__Button'>
-            2
-          </button>
-          <button className='controlPanel__Number controlPanel__Button'>
-            1
-          </button>
-          <button className='controlPanel__Number controlPanel__Button'>
-            <img
-              className='controlPanel__ImgButton'
-              src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAQElEQVR4nO3RQQoAEAAF0Tke7r+m5B6sbGVBKfPqr2fxQZKkTQkoQD+8AsRVuF6IzrUnwxHIF6IZCLt/S5I+NwDHN1vXMmzw6wAAAABJRU5ErkJggg=='
-            />
-          </button>
-          <button
-            className='controlPanel__Number controlPanel__Button'
-            disabled='disabled'
-            style={{ background: 'transparent', border: 'transparent' }}
-          />
-          <button className='controlPanel__Number controlPanel__Button'>
-            0
-          </button>
-          <button
-            className='controlPanel__Number controlPanel__Button'
-            disabled='disabled'
-            style={{ background: 'transparent', border: 'transparent' }}
-          />
-          <button className='controlPanel__Number controlPanel__Button'>
-            <img
-              className='controlPanel__ImgButton'
-              src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAAASElEQVR4nO3UMQrAIBAF0Tle3PvXCsF7xMrWQrJgwjz49RQLC5L+JoAGPC+vAWUVvhOic/3IcAFqQrQC1+79pW8KPxd+LkmnGYBPt60ph2azAAAAAElFTkSuQmCC'
-            />
-          </button>
+        <div className='controlPanel row'>
+          <div className='numbers col-8'>
+            <Buttons printNumber={printNumber} />
+          </div>
+          <div className='operatos col-4'>
+            <Operators />
+          </div>
         </div>
       </div>
     </div>
